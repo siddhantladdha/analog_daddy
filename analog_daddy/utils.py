a796221da688db6de4857a33a7794663f253d7af
Siddhant Laddha <25705822+siddhantladdha@users.noreply.github.com>
2023-11-05 15:31:21 -0500

    added the basic skeleton of look_upW function

--- analog_daddy/utils.py
+++ analog_daddy/utils.py
@@ -3,6 +3,8 @@ This module contains additional utility functions that can be used
 to improve code quality.
 """
 import numpy as np
+from analog_daddy.look_up import look_up
+from analog_daddy.conf import * # import all the config variables
 
 def pretty_print_structure(d, indent=0):
     """Pretty print the structure of a dictionary containing numpy arrays and strings."""
@@ -34,3 +36,45 @@ def describe_structure(d):
         return f'string: "{d}"'
     else:
         return type(d).__name__
+
+def look_upW(transistor,length_increment=10e-9):
+    """
+    Look up the value of a device width for the
+    given gm_id, length, and id.
+    Modify the transistor dictionary in place.
+    The transistor dictionary should have the following
+    transistor = {
+        'type': nmos, # actual dictionary of the device LUT
+        'length': L_MIN, # starting value of the length
+        'w': W_MIN, # does not matter, will be overwritten
+        "id": 10e-6, # current value to look up the width for.
+        "gm_id" : 15 # gm_id value to look up the width for.
+    }
+    If the width is less than the minimum width,
+    start increasing the length until the width
+    is greater than the minimum width.
+    We don't put checks for maximum width because
+    we can stack it and most likely this iteration
+    is required in strong inversion cases where
+    we won't increase the width anyway.
+    """
+    w_calc = transistor['id']/look_up(transistor['type'],
+                                      'id_w', gm_id=transistor['gm_id'],
+                                      length=transistor['length'])
+    while (w_calc < W_MIN) and (transistor['length'] < L_MAX):
+        print(f"""A gm_id of {transistor['gm_id']} cannot be achieved for the length:
+              {transistor['length']} and id: {transistor['id']}. Increasing the length.""")
+        if IS_INTEGER:
+            transistor['length'] += 1
+        else:
+            transistor['length'] += length_increment
+        w_calc = transistor['id']/look_up(transistor['type'],
+                                        'id_w', gm_id=transistor['gm_id'],
+                                        length=transistor['length'])
+
+    transistor['w'] = round(w_calc) if IS_INTEGER else w_calc
+    if transistor['length'] >= L_MAX:
+        print(f"""The length has reached the maximum length of {L_MAX} for a gm_id of
+              {transistor['gm_id']} and id of {transistor['id']}.""")
+        return False
+    return True
