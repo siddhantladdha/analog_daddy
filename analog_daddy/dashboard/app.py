9b284ff49f684afc1e980cb48e2c78e54deef8b4
Siddhant Laddha <25705822+siddhantladdha@users.noreply.github.com>
2025-07-02 18:15:40 -0400

    refactoring the input table.

--- analog_daddy/dashboard/app.py
+++ analog_daddy/dashboard/app.py
@@ -4,6 +4,7 @@ import numpy as np
 from parse_si import format_si_or_scientific as fmt_str_si
 from sidebar import render_sidebar
 from analog_daddy.look_up import look_up
+from selection_tables import device_selection_table, variable_selection_table, input_range_table
 
 st.set_page_config(
     page_title="Analog Daddy Dashboard",
@@ -35,126 +36,28 @@ for msg in status_msgs:
     else:
         st.info(msg)
 
-# region Device Selection Table
-headers = ["Device Type", "Temperature/Corner", "Info"]
-st.markdown("## Device Selection Table")
+device_selection_table(lut_metadata)
+variable_selection_table(lut_metadata[0], st.session_state.get("selected_device_type_0"))
 
-# Prepare table: first row is headers, then the data rows
-table = [headers] + [
-    [lut_metadata_entry["Device Type"],
-     lut_metadata_entry["Temperature/Corner"],
-     lut_metadata_entry["Info"]]
-    for lut_metadata_entry in lut_metadata
-]
-
-column_widths = [1, 1, 1]  # Adjust widths for each column
-for row_idx, row_value in enumerate(table):
-    columns = st.columns(column_widths, border=True, gap="small")
-    for col_idx, (column, cell) in enumerate(zip(columns, row_value)):
-        if row_idx == 0: # Header row
-            column.markdown(f"**{cell}**")
-        else: # Data rows
-            if col_idx == 0:  # Device Type column
-                selected = column.selectbox(
-                    label="Lable_text",
-                    options=lut_metadata[row_idx - 1]["Device Type"],
-                    key=f"selected_device_type_{row_idx - 1}",
-                    label_visibility="collapsed",
-                    index=0
-                )
-            else:
-                column.write(cell)
-# endregion
-
-# region Variable Selection and Display
-st.markdown("## Variable Selection and Display")
-# --- Mutually Exclusive Selection for Independent Vars using st.multiselect ---
-selected_independent_var = st.multiselect(
-    textwrap.dedent("""\
-                    **Independent variables**\n
-                    Choose upto two variables.\n
-                    The **first** option is used as the **x-axis**.\n
-                    The **second** option is used for the **parametric axis**."""),
-    # Use the first LUT's independent variables for the selectbox
-    # Check is anyway performed to make sure the independent variables
-    # are the same for both LUTs.
-    list(
-        lut_metadata[0]["independent_vars"][
-            st.session_state.get("selected_device_type_0")
-            ].keys()),
-    default=None,
-    key="selected_independent_var",
-    max_selections=2
-)
-
-selected_dependent_var = st.selectbox(
-                                    "**Dependent variable**",
-                                    lut_metadata[0]["dependent_vars"][
-                                    st.session_state.get("selected_device_type_0")],
-                                    index=None,
-                                    key="selected_dependent_var"
-                                    )
-
-if selected_dependent_var and selected_independent_var:
-    if selected_dependent_var in selected_independent_var:
-        st.error(
+# Mutual exlusivity check for independent and dependent variables
+if (
+    st.session_state.get("selected_independent_var") and
+    st.session_state.get("selected_dependent_var") and
+    st.session_state.get("selected_dependent_var") in
+    st.session_state.get("selected_independent_var")
+):
+    st.error(
             (
             "Dependent variable cannot be an independent variable. "
             "Please select a different variable."
             ))
-        st.stop()
-# endregion
+    st.stop()
 
-var_default = { }
-# create input fields for the independent variables.
-# Assume 'selected' is your list of selected independent variables from st.multiselect
-if selected_independent_var:
-    for idx, var in enumerate(selected_independent_var):
-        st.markdown(f"**Range for {var}:**")
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            start = st.text_input(
-                    f"{var} start",
-                    key=f"{var}_start",
-                    value=fmt_str_si(
-                    min(
-                    lut_metadata[j]["independent_vars"][
-                    st.session_state.get(f"selected_device_type_{j}")
-                    ][var]["min"]
-                    for j in range(len(lut_metadata))
-                    )))
-        with col2:
-            stop = st.text_input(
-                    f"{var} stop",
-                    key=f"{var}_stop",
-                    value=fmt_str_si(
-                    min(
-                    lut_metadata[j]["independent_vars"][
-                    st.session_state.get(f"selected_device_type_{j}")
-                    ][var]["max"]
-                    for j in range(len(lut_metadata))
-                    )))
-        with col3:
-            # Use the idx to determine between X-var and Parametric var
-            # we don't bother comparing step size/ elements between two LUTs, just use the first one
-            if st.session_state.get(f"var_step_mode_selector_{idx}") == "Start:Stop:Step Mode":
-                step = st.text_input(
-                f"{var} step",
-                key=f"{var}_step",
-                value=fmt_str_si(
-                lut_metadata[0]["independent_vars"][
-                st.session_state.get("selected_device_type_0")
-                ][var]["step"]
-                ))
-            elif st.session_state.get(
-                f"var_step_mode_selector_{idx}"
-                ) == "Start:Stop:N-Elements Mode":
-                # In this mode, we don't use step, but rather the number of elements
-                n_elements = st.text_input(
-                f"{var} N-elements",
-                key=f"{var}_n_elements",
-                value=fmt_str_si(
-                lut_metadata[0]["independent_vars"][
-                st.session_state.get("selected_device_type_0")
-                ][var]["N-elements"]
-                ))
+input_range_table(
+    lut_metadata,
+    [
+        st.session_state.get("selected_device_type_0"),
+        st.session_state.get("selected_device_type_1"),
+    ],
+    st.session_state.get("selected_independent_var"),
+    )
---
+++ analog_daddy/dashboard/selection_tables.py
@@ -0,0 +1,112 @@
+import textwrap
+import streamlit as st
+from parse_si import format_si_or_scientific as fmt_str_si
+
+def device_selection_table(lut_metadata=None):
+    """
+    Render the device selection table using Streamlit columns and selectboxes.
+    """
+    headers = ["Device Type", "Temperature/Corner", "Info"]
+    table = [headers] + [
+        [lut_metadata_entry["Device Type"],
+         lut_metadata_entry["Temperature/Corner"],
+         lut_metadata_entry["Info"]]
+        for lut_metadata_entry in lut_metadata
+    ]
+    column_widths = [1, 1, 1]
+    for row_idx, row_value in enumerate(table):
+        columns = st.columns(column_widths, border=True, gap="small")
+        for col_idx, (column, cell) in enumerate(zip(columns, row_value)):
+            if row_idx == 0:
+                column.markdown(f"**{cell}**")
+            else:
+                if col_idx == 0:
+                    column.selectbox(
+                        label="Lable_text",
+                        options=lut_metadata[row_idx - 1]["Device Type"],
+                        key=f"selected_device_type_{row_idx - 1}",
+                        label_visibility="collapsed",
+                        index=0
+                    )
+                else:
+                    column.write(cell)
+    return 0
+
+def variable_selection_table(lut_metadata_elem=None, selected_device_type_elem=None):
+    """
+    Render the variable selection controls for independent and dependent variables.
+    Take only one element of the lut_metadata and corresponding selected_device_type
+    which is obtained using the session_state
+    Mutual exclusivity is maintained for independent variables
+    using st.multiselect.
+    Mutual exclusivity between the independent and the dependent variable
+    is maintained at top-level using st.stop()
+    """
+    st.multiselect(
+        textwrap.dedent("""\
+                        **Independent variables**
+                        (Choose upto two variables)\n
+                        **First** option is the **x-axis**.\n
+                        **Second** option is the **parametric axis**."""),
+        list(
+            lut_metadata_elem["independent_vars"][selected_device_type_elem].keys()
+        ),
+        default=None,
+        key="selected_independent_var",
+        max_selections=2
+    )
+    st.selectbox(
+        "**Dependent variable**",
+        lut_metadata_elem["dependent_vars"][selected_device_type_elem],
+        index=None,
+        key="selected_dependent_var"
+    )
+    return 0
+
+def input_range_table(lut_metadata=None, selected_device_type=None, selected_independent_var=None):
+    """
+    Render the input range controls for the selected independent variables.
+    Returns a dictionary of default values for each variable.
+    """
+    st.markdown("**Range for variables:**")
+    if (lut_metadata and
+        selected_device_type and
+        selected_independent_var):
+
+        for idx, var in enumerate(selected_independent_var):
+            # Setting the correct key for the step mode selector
+            if st.session_state.get(f"var_step_mode_selector_{idx}") == "Start:Stop:Step Mode":
+                var_step_mode_key = "step"
+            elif st.session_state.get(f"var_step_mode_selector_{idx}") == "Start:Stop:N-Elements Mode":
+                var_step_mode_key = "N-elements"
+            else:
+                var_step_mode_key = "step"
+
+            col1, col2, col3 = st.columns(3)
+            col1.text_input(
+                f"{var} start",
+                key=f"{var}_start",
+                value=fmt_str_si(
+                    min(
+                        lut_met_val["independent_vars"][selected_device_type[j]][var]["min"]
+                        for j, lut_met_val in enumerate(lut_metadata))
+                    )
+                )
+            col2.text_input(
+                f"{var} stop",
+                key=f"{var}_stop",
+                value=fmt_str_si(
+                    min(
+                        lut_met_val["independent_vars"][selected_device_type[j]][var]["max"]
+                        for j, lut_met_val in enumerate(lut_metadata))
+                    )
+                )
+            col3.text_input(
+                f"{var} {var_step_mode_key}",
+                key=f"{var}_{var_step_mode_key}",
+                value=fmt_str_si(
+                    lut_metadata[0]["independent_vars"][
+                        selected_device_type[0]][var][var_step_mode_key]
+                )
+            )
+    return 0
